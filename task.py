9c1b6090da29653988de39cc0d90bb63074cb5f7
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-16 20:05:41 +0700

    Tip Calculator project

--- task.py
+++ task.py
@@ -1,8 +1,17 @@
 # BMI Calculator
 
-weight=float(input('Enter your weight\n'))
-height=float(input('Enter your height\n'))
-bmi=weight/(height**2)
-print('BMI is ', bmi)
+# weight=float(input('Enter your weight\n'))
+# height=float(input('Enter your height\n'))
+# bmi=weight/(height**2)
+# print('BMI is ', bmi)
 
+
+#Tip Calculator
+
+print('Welcome to the tip calculator!')
+total=float(input('What was the total bill? $'))
+tip=float(input('How much tip would you like to give?'))
+people=int(input('How many people to split the bill?'))
+result=(total+(total*0.12))/people
+print('Each person should pay: $',round(result,2))
 
