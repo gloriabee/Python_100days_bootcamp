e1061c739d5d07fd7f170d9ac14c068c66d76f35
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-16 12:01:02 +0700

    Band Name Generator (D1:project)

--- task.py
+++ task.py
@@ -1,6 +1,4 @@
-print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
-print('2. Knead the dough for 10 minutes.')
-print('3. Add 3g of Salt.')
-print('4. Leave to rise for 2 hours.')
-print('5. Bake at 200 degrees C for 30 minutes.')
-
+print('Welcome to the Band Name Generator.')
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print('Your band name could be '+city+' '+pet)
