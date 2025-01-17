93dfb503134cb11456bc376f9db139e2bea499fe
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-17 16:44:11 +0700

    if else conditions

--- task.py
+++ task.py
@@ -1,17 +1,7 @@
-# BMI Calculator
+print('Welcome to the rollercoaster!')
+height=int(input('What is your height in cm?'))
 
-# weight=float(input('Enter your weight\n'))
-# height=float(input('Enter your height\n'))
-# bmi=weight/(height**2)
-# print('BMI is ', bmi)
-
-
-#Tip Calculator
-
-print('Welcome to the tip calculator!')
-total=float(input('What was the total bill? $'))
-tip=float(input('How much tip would you like to give?'))
-people=int(input('How many people to split the bill?'))
-result=(total+(total*0.12))/people
-print('Each person should pay: $',round(result,2))
-
+if height==120:
+    print('Can ride')
+else:
+    print('Cannot ride')
