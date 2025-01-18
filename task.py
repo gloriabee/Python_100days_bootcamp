2ce7cf15e3151a6f9ef087e6c33d60d1e8000af7
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-18 11:08:58 +0700

    Coin flip using random module

--- task.py
+++ task.py
@@ -1,20 +1,6 @@
-# Treasure Island Game
-
-print("Welcome to Treasure Island.\n Your mission is to find the treasure. \n You're at across road. Where do you want to go?\n")
-direction=input(' Type \"left\" or \"right\"\n')
-if(direction=='left'):
-    print("You've come to a lake. There is an island in the middle of the lake.")
-    action=input('Type "wait" to wait for a boat. Type "swim" to swim across.\n')
-    if(action=='wait'):
-        print('You arrive at the island unharmed. There is a house with 3 doors.\nOne red, one yellow and one blue.')
-        color=input('Which color do you choose?\n')
-        if(color=='red'):
-            print("It's a room full of fire. Game Over.")
-        elif(color=='blue'):
-            print('You enter a room of beasts. Game Over')
-        elif(color=='yellow'):
-            print('You found the treasure! You Win!')
-    else:
-        print('You get attacked by an angry trout. Game Over.')
+import random
+random_coin=random.randint(0,1)
+if(random_coin==0):
+    print('Head')
 else:
-    print('You fell into a hole. Game Over')
+    print('Tail')
