3f31a85f9bb649f9d0f7254086a41e8009433430
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-18 12:52:13 +0700

    rock/paper/scissor project

--- task.py
+++ task.py
@@ -1,10 +1,50 @@
 import random
+rock='''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper='''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-# My Solution 
-friends=['Alice','Gloria','AC','Rubi']
-random=random.randint(0,len(friends)-1)
-print(friends[random])
+scissor='''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+my_choice=int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
+com_choice= random.randint(0,2)
+if(my_choice==0):
+    print(rock)
+elif(my_choice==1):
+    print(paper)
+else:
+    print(scissor)
+print('Computer choose: ')
+if(com_choice==0):
+    print(rock)
+elif(com_choice==1):
+    print(paper)
+else:
+    print(scissor)
 
-# Option 1
-print(random.choice(friends))
+if(my_choice==com_choice):
+    print("It's a draw.")
+elif((my_choice==0 and com_choice==2) or (my_choice==2 and com_choice==1) or (my_choice==1 and com_choice==0)):
+    print('You Win!')
+else:
+    print('You lose')
+
 
