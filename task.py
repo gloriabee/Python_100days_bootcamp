f7b3f61c922f93ce5b9412124c0526c8c35ace89
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-20 17:41:19 +0700

    debugging maze game

--- task.py
+++ task.py
@@ -2,17 +2,16 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
-def jump():
-    turn_left()
-    
+ 
+while front_is_clear() and right_is_clear():
+    move()
+
 while not at_goal():
-    if front_is_clear():
-        move()
-    elif right_is_clear():
-        turn_right()
-    else:
-        jump()
-    
+        if front_is_clear():
+            move()
+        elif right_is_clear():
+            turn_right()
+        else:
+            turn_left()
    
    
