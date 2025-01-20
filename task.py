c4b48d92e75d3c5fe22548a0e55763b7087de3bb
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-20 16:38:43 +0700

    Maze game using function and conditions

--- task.py
+++ task.py
@@ -1,37 +1,18 @@
-#Password Generator Project
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-no_random=[]
-for i in range(0,nr_letters):
-    no_random.append(random.choice(letters))
-
-for j in range(0,nr_symbols):
-    no_random.append(random.choice(symbols))
-
-for k in range(0,nr_numbers):
-    no_random.append(random.choice(numbers))
-
-print(no_random)
-random.shuffle(no_random)
-print(no_random)
-
-password=''
-for num in no_random:
-  password+=num
-print('Your password is '+password)
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
     
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+def jump():
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+    else:
+        jump()
+    
+   
+   
