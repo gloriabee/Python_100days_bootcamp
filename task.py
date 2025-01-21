62db4b5beb77a71fae3b7cdb08d5e454201bbc2d
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-22 02:19:20 +0700

    replacing words

--- task.py
+++ task.py
@@ -3,28 +3,41 @@ word_list=['aardvark','baboon','camel']
 
 #Todo-1 Randomly Choose a word from the word_list and assignment
 random_word=random.choice(word_list)
+print(random_word)
 
 i=0
+result=''
 print('word to guess: ',end='')
 while i<len(random_word):
-    print('-',end='')
+    result+='-'
     i+=1
-print()
+print(result)
 
 #Todo-2 Ask the user to guess a letter and assign their answer
-life=1
-answer=''
+# life=1
+# answer=''
 
-for life in range(6):
- while(answer!=random_word):
-    letter=input('Guess a letter:')
-    if letter in random_word:
-        i=random_word.index(letter)
-        answer+=random_word[i]
-    else:
-        print('wrong')
-life-=1
+# for life in range(6):
+#  while(answer!=random_word):
+#     letter=input('Guess a letter:')
+#     if letter in random_word:
+#         i=random_word.index(letter)
+#         answer+=random_word[i]
+#         print('Right')
+#     else:
+#         print('wrong')
+# life-=1
 
+guess=input('Guess a letter: ').lower()
+print(guess)
 
-
+result=''
 #Todo-3 Check if the letter the user guessed (guess) is one is wrong if
+for i,char in enumerate(random_word):
+    if char==guess:
+         print(char,end='')
+    else:
+         print('-',end='')
+
+
+    
