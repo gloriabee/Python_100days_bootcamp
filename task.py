8a3523277cfb80adb433a7e0d8eaf3ae041912f3
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-22 21:41:33 +0700

    hangman game final version

--- task.py
+++ task.py
@@ -1,43 +1,53 @@
+# Start
 import random
-word_list=['aardvark','baboon','camel']
 
+from hangman_words import word_list
+from hangman_art import stages,logo
+
+print(logo)
 #Todo-1 Randomly Choose a word from the word_list and assignment
 random_word=random.choice(word_list)
-print(random_word)
 
-i=0
-result=''
-print('word to guess: ',end='')
-while i<len(random_word):
-    result+='-'
-    i+=1
-print(result)
 
 #Todo-2 Ask the user to guess a letter and assign their answer
-# life=1
-# answer=''
+placeholder='Word to guess: '
+for position in range(len(random_word)):
+    placeholder+='_'
+print(placeholder)
 
-# for life in range(6):
-#  while(answer!=random_word):
-#     letter=input('Guess a letter:')
-#     if letter in random_word:
-#         i=random_word.index(letter)
-#         answer+=random_word[i]
-#         print('Right')
-#     else:
-#         print('wrong')
-# life-=1
+        
+game_over=False
+correct_letters=[]
+life=6
 
-guess=input('Guess a letter: ').lower()
-print(guess)
+while not game_over:
+   
+   guess=input('Guess a letter: ').lower()
+   if guess in correct_letters:
+       print(f"You've already guessed {guess}")
+   display="Word to guess: "
+   for letter in random_word:
+        if letter==guess:
+            display+=letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display+="_"
+   print(display)
+   
+   if guess not in random_word:
+       life-=1
+       print(f"You guessed {guess}, that is not in the word. You lose a life.")
+       if life==0:
+            game_over=True
+            print(f"You wrong! That was {letter}")
+   if "_" not in display:
+        game_over=True
+        print("You win.")
+   print('You have left ',life,' lives ')
+   print(stages[life])
 
-result=''
-#Todo-3 Check if the letter the user guessed (guess) is one is wrong if
-for i,char in enumerate(random_word):
-    if char==guess:
-         print(char,end='')
-    else:
-         print('-',end='')
 
+  
 
-    
