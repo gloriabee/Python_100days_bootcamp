6e1f7aa64620f196c2db4a916714c0cce594d555
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-24 01:15:59 +0700

    my own encrypt

--- task.py
+++ task.py
@@ -1,24 +1,51 @@
 import string
-def cipher(choice):
-   msg=input('Type your message:\n')
-   shift=int(input('Type the shift number:\n'))
-   if(choice=='encode'):
-    print("Here's the encoded result: ")
-   else:
-    print("Here's the decoded result: ") 
-
 alphabet=string.ascii_lowercase+string.ascii_uppercase+string.punctuation
-choice=input("Type 'encode' to encrypt,type 'decode' to decrypt:\n")
-isEnd=False
-while not isEnd:
-  cipher(choice)
-  decision=input("Type 'yes' if you want to go again, Otherwise type 'no'\n")
-  if(decision=='no'):
-    isEnd=True
-  else:
-    isEnd=False
-
 print(alphabet)
+print(len(alphabet))
+# choice=input("Type 'encode' to encrypt,type 'decode' to decrypt:\n")
+# msg=input('Type your message:\n')
+# shift=int(input('Type the shift number:\n'))
+
+# Todo-1: Create a function called encrypt() that takes 'original_text' and 'shift_amount' as 2 inputs. 
+result=''
+textList=[]
+def encrypt(msg,shift):
+  for char in msg:
+    currentI=alphabet.index(char)
+    nextI=currentI+shift
+    if nextI==len(alphabet):
+       currentI=len(alphabet)-nextI
+       textList.append(alphabet[currentI])
+       result=''.join(textList)
+    elif nextI>len(alphabet):
+       currentI=nextI-len(alphabet)
+       textList.append(alphabet[currentI])
+       result=''.join(textList)
+    else:
+        currentI=currentI+shift
+        textList.append(alphabet[currentI])
+        result=''.join(textList)
+  print(result)
+  
+encrypt('}~}~',1)
+# def cipher(choice):
+  
+#    if(choice=='encode'):
+#     print("Here's the encoded result: ")
+#    else:
+#     print("Here's the decoded result: ") 
+
+
+
+# isEnd=False
+# while not isEnd:
+#   cipher(choice)
+#   decision=input("Type 'yes' if you want to go again, Otherwise type 'no'\n")
+#   if(decision=='no'):
+#     isEnd=True
+#   else:
+#     isEnd=False
+# print(alphabet)
     
 
 
