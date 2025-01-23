1bc18f07e818a8eec95cec152e0cb45df36a9d34
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-24 02:08:54 +0700

    Caesor Cipher Game Final Version

--- task.py
+++ task.py
@@ -1,51 +1,40 @@
 import string
 alphabet=string.ascii_lowercase+string.ascii_uppercase+string.punctuation
-print(alphabet)
-print(len(alphabet))
-# choice=input("Type 'encode' to encrypt,type 'decode' to decrypt:\n")
-# msg=input('Type your message:\n')
-# shift=int(input('Type the shift number:\n'))
 
-# Todo-1: Create a function called encrypt() that takes 'original_text' and 'shift_amount' as 2 inputs. 
-result=''
-textList=[]
+# encrypt function
 def encrypt(msg,shift):
+  cipher_text=""
   for char in msg:
-    currentI=alphabet.index(char)
-    nextI=currentI+shift
-    if nextI==len(alphabet):
-       currentI=len(alphabet)-nextI
-       textList.append(alphabet[currentI])
-       result=''.join(textList)
-    elif nextI>len(alphabet):
-       currentI=nextI-len(alphabet)
-       textList.append(alphabet[currentI])
-       result=''.join(textList)
-    else:
-        currentI=currentI+shift
-        textList.append(alphabet[currentI])
-        result=''.join(textList)
-  print(result)
-  
-encrypt('}~}~',1)
-# def cipher(choice):
-  
-#    if(choice=='encode'):
-#     print("Here's the encoded result: ")
-#    else:
-#     print("Here's the decoded result: ") 
+    shifted_position=alphabet.index(char)+shift
+    shifted_position%=len(alphabet)
+    cipher_text+=alphabet[shifted_position]
+  print(f"Here is the encoded result: {cipher_text}")
 
 
+# decrypt 
+def decrypt(msg,shift):
+   cipher_text=""
+   for char in msg:
+      shifted_position=alphabet.index(char)-shift
+      cipher_text+=alphabet[shifted_position]
+   print(f"Here is the decoded result: {cipher_text}")
 
-# isEnd=False
-# while not isEnd:
-#   cipher(choice)
-#   decision=input("Type 'yes' if you want to go again, Otherwise type 'no'\n")
-#   if(decision=='no'):
-#     isEnd=True
-#   else:
-#     isEnd=False
-# print(alphabet)
+
+isEnd=False
+while not isEnd:
+     choice=input("Type 'encode' to encrypt,type 'decode' to decrypt:\n").lower()
+     msg=input('Type your message:\n').lower()
+     shift=int(input('Type the shift number:\n'))
+     if(choice=='encode'):
+        encrypt(msg,shift)
+     else:
+        decrypt(msg,shift)
+     decision=input("Type 'yes' if you want to go again, Otherwise type 'no'\n")
+     if(decision=='no'):
+        isEnd=True
+     else:
+        isEnd=False
+
     
 
 
