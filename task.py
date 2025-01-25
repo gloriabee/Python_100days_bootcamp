5085dd197ce0904f500d8de94687b6859f2208f2
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-25 12:10:08 +0700

    functions with outputs

--- task.py
+++ task.py
@@ -1,36 +1,4 @@
-import os
-users={}
-bids=[]
-isEnd=False
-#Solution 2
-def find_highest_bidder(bidding_dictionary):
-   highest_bid=0
-   for bidder in bidding_dictionary:
-      bid_amount=bidding_dictionary[bidder]
-      if bid_amount>highest_bid:
-         highest_bid=bid_amount
-         winner=bidder
-   print(f'The winner is {winner} with a bid of {highest_bid}')  
-   
+def format_name(f_name,l_name):
+   return f_name.title()+' '+l_name.title()
 
-while not isEnd:
-   # Ask the user for input
-   name=input('What is your name?: ')
-   bid=int(input('What\'s your bid?: $'))
-   # Save data into dictionary
-   users[name]=bid
-   # Whether if new bids need to be added
-   decision=input('If other users who want to bid? ').lower()
-   if decision=='yes':
-      os.system('cls')
-   else:
-      os.system('cls')
-      isEnd=True
-      find_highest_bidder(users)
-
-   
-
-   
-   
-
-
+print(format_name('gloria win','nyunt'))
