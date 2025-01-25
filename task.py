f297436eec7ddac92d3fcceec7398eb0f582333a
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-25 11:51:15 +0700

    Solution 2 for blind auction project

--- task.py
+++ task.py
@@ -2,25 +2,35 @@ import os
 users={}
 bids=[]
 isEnd=False
+#Solution 2
+def find_highest_bidder(bidding_dictionary):
+   highest_bid=0
+   for bidder in bidding_dictionary:
+      bid_amount=bidding_dictionary[bidder]
+      if bid_amount>highest_bid:
+         highest_bid=bid_amount
+         winner=bidder
+   print(f'The winner is {winner} with a bid of {highest_bid}')  
+   
+
 while not isEnd:
+   # Ask the user for input
    name=input('What is your name?: ')
    bid=int(input('What\'s your bid?: $'))
+   # Save data into dictionary
    users[name]=bid
-   for name in users:
-      bids.append(users[name])
-   maxBid=max(bids)
-   for name in users:
-      if users[name]==maxBid:
-         winner=name
+   # Whether if new bids need to be added
    decision=input('If other users who want to bid? ').lower()
    if decision=='yes':
       os.system('cls')
-      isEnd=False
    else:
+      os.system('cls')
       isEnd=True
-print(f'The winner is {winner} with a bid of {maxBid}')  
+      find_highest_bidder(users)
+
    
 
+   
    
 
 
