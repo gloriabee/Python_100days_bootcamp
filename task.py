9fc40c4a5bfb8af057d7950d531c74c53d393b82
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-25 11:20:55 +0700

    secret auction program

--- task.py
+++ task.py
@@ -1,26 +1,26 @@
-student_scores = {
-    'Harry': 88,
-    'Ron': 78,
-    'Hermione': 95,
-    'Draco': 75,
-    'Neville': 60
-}
-student_grades={}
-
-def score_to_grade(score):
-   if(score>90 and score<=100):
-      grade='Outstanding'
-   elif(score>80 and score<=90):
-      grade='Exceeds Expectations'
-   elif(score>70 and score<=80):
-      grade='Acceptable'
+import os
+users={}
+bids=[]
+isEnd=False
+while not isEnd:
+   name=input('What is your name?: ')
+   bid=int(input('What\'s your bid?: $'))
+   users[name]=bid
+   for name in users:
+      bids.append(users[name])
+   maxBid=max(bids)
+   for name in users:
+      if users[name]==maxBid:
+         winner=name
+   decision=input('If other users who want to bid? ').lower()
+   if decision=='yes':
+      os.system('cls')
+      isEnd=False
    else:
-      grade='Fail'
-   return grade
-      
+      isEnd=True
+print(f'The winner is {winner} with a bid of {maxBid}')  
+   
 
-for name in student_scores:
-   score=student_scores[name]
-   student_grades[name]=score_to_grade(score)
+   
 
-print(student_grades)
+
