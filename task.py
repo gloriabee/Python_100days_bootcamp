15603b8170b5eef9081d50c5e8cf53bf48f6f874
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-28 11:51:45 +0700

    Black Jack Game

--- task.py
+++ task.py
@@ -1,62 +1,70 @@
+import random
 import os
-def add(num1,num2):
-   num1=num1+num2
-   return num1
-def sub(num1,num2):
-   num1=num1-num2
-   return num1
-def mul(num1,num2):
-   num1= num1*num2
-   return num1
-def div(num1,num2):
-   num1=num1/num2
-   return num1
 
-logo = '''
- _____________________
-|  _________________  |
-| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
-| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
-|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
-| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \\     | || |  |_   _|     | || |   .' ___  |  | |
-| |___|___|___| |___| | | |  / .'   \\_|  | || |    / /\\ \\    | || |    | |       | || |  / .'   \\_|  | |
-| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \\   | || |    | |   _   | || |  | |         | |
-| |___|___|___| |___| | | |  \\ '.___.'\\  | || | _/ /    \\ \\_ | || |   _| |__/ |  | || |  \\ '.___.'\\  | |
-| | 1 | 2 | 3 | | x | | | |   '._____.'  | || ||____|  |____|| || |  |________|  | || |   '._____.'  | |
-| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
-| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
-| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
-|_____________________|
-'''
-print(logo)
-result=0
+# Create a deal_card() function that uses the List below to return a random card
+def deal_card():
+    cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+    return random.choice(cards)
+    
+# calculate_score()
+def calculate_score(cards):
+    if sum(cards)==21 and len(cards)==2:
+        return 0
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
-isContinue=True
-while isContinue:
-   #Asking first number
-   num1=int(input("What's the first number?: "))
-   isEnd=False
-   while not isEnd:
-      #Asking operator
-      print('+\n-\n*\n/\n')
-      operation=input('pick an operation: ')
-      #Asking second number
-      num2=int(input("What's the next number: "))
-      if(operation=='+'):
-         result=add(num1,num2)
-      elif(operation=='-'):
-         result=sub(num1,num2)
-      elif(operation=='*'):
-         result=mul(num1,num2)
-      else:
-         result=div(num1,num2)
-      print(f"{num1} {operation} {num2} = {result}")
-      num1=result
-      decision=input("Type 'y' to continue calculating with 15.0 or type 'n' to start a new calculation: ").lower()
-      if decision=='n':
-         isEnd=True
-         os.system('cls')
-      else:
-         isContinue=True
+def compare(user_score,computer_score):
+    if user_score==computer_score:
+        return 'draw'
+    elif computer_score==0:
+        return "Lose,opponent has Blackjack"
+    elif user_score==0:
+        return "Win With a blackjack"
+    elif user_score>21:
+        return "You went over, You lose"
+    elif computer_score>21:
+        return "Opponent went over. You win"
+    elif user_score>computer_score:
+        return 'You win'
+    else:
+        return 'You lose'
 
+## Deal user and computer 2 cards
+def playGame():
+      user_cards=[]
+      computer_cards=[]
+      computer_score=-1
+      user_score=-1
+      isGameOver=False
 
+      for _ in range(2):
+         user_cards.append(deal_card())
+         computer_cards.append(deal_card())
+
+      while not isGameOver: 
+         user_score=calculate_score(user_cards)
+         computer_score=calculate_score(computer_cards)
+         print(f"Your cards {user_cards}, current score: {user_score}")
+         print(f"Computer's first cards: {computer_cards[0]}")
+         if user_score==0 or computer_score==0 or user_score>21:
+            isGameOver=True
+         else: 
+            user_should_deal=input("Type 'y' to get another card, type 'n' to pass: ").lower()
+            if user_should_deal=='y':
+               user_cards.append(deal_card())
+            else:
+               isGameOver=True
+
+      while computer_score!=0 and computer_score<17:
+         computer_cards.append(deal_card())
+         computer_score=calculate_score(computer_cards)
+
+      print(f"Your Final Hand: {user_cards}, final score: {user_score}")
+      print(f"Computer's Final Hand: {computer_cards}, final score: {computer_score}")
+      print(compare(user_score,computer_score))
+    
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n':")=="y":
+    os.system('cls')
+    playGame()
