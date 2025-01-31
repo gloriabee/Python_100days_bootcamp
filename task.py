2147de08714a6609563279a5c84ce6cfc961987a
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-01-31 23:13:14 +0700

    prime checker

--- task.py
+++ task.py
@@ -1,36 +1,10 @@
-import random
-
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-random_number=random.randint(1,100)
-print(f"The correct answer is {random_number}")
-level=input("Choose a difficulty. Type 'easy' or 'hard':").lower()
-
-def checkNum(life):
-    isEnd=False
-    while not isEnd and life>0:
-        print(f"You have {life} attempts remaining to guess the number.")
-        guess=int(input('Make a guess: '))
-        if(guess<random_number):
-            print('Too Low.')
-            life-=1
-            print('Guess again.')
-        elif(guess>random_number):
-            print('Too high')
-            life-=1
-            print('Guess again.')
-        elif(guess==random_number):
-            print(f"You got it! The answer was {random_number}")
-            isEnd=True
-        else:
-            print('You lost')
-
-    
-if level=='easy':
-    life=10
-    checkNum(life)
-elif level=='hard':
-    life=5
-    checkNum(life)
-else:
-    print('Chose wrong level:')
+def is_prime(num):
+    if num==2:
+        return True
+    if num==1:
+        return False
+    for i in range(2,num):
+        if num%i==0:
+            return False
+    return True
+print(is_prime(7))
