5181080b07761d0893ba0f30276684898bc2a775
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-02-07 23:01:58 +0700

    Higher lower game

---
+++ game_data.py
@@ -0,0 +1,123 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count": 600000000,
+        "description": "Social media platform",
+        "country": "United States"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 500000000,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 300000000,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 250000000,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 270000000,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400000000,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 350000000,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 250000000,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Neymar",
+        "follower_count": 200000000,
+        "description": "Footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "National Geographic",
+        "follower_count": 200000000,
+        "description": "Magazine",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 280000000,
+        "description": "Musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 220000000,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 230000000,
+        "description": "Reality TV personality and model",
+        "country": "United States"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "follower_count": 180000000,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Nicki Minaj",
+        "follower_count": 170000000,
+        "description": "Musician",
+        "country": "Trinidad and Tobago"
+    },
+    {
+        "name": "Nike",
+        "follower_count": 220000000,
+        "description": "Sportswear multinational",
+        "country": "United States"
+    },
+    {
+        "name": "Khloé Kardashian",
+        "follower_count": 210000000,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Miley Cyrus",
+        "follower_count": 160000000,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Katy Perry",
+        "follower_count": 150000000,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 140000000,
+        "description": "Actress and musician",
+        "country": "United States"
+    }
+]
+
--- task.py
+++
@@ -1,8 +0,0 @@
-try:
-    age=int(input('How old are you?'))
-except ValueError:
-    print("You typed invalid number. please try again")
-    age=int(input('How old are you?'))
-
-    if(age>18):
-        print(f'You can drive at age {age}')
